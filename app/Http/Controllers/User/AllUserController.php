fb4cdef8ea44d44323391da50b3aa847961f70c2
unknown <mahmoudhamdyam@gmail.com>
2023-07-31 11:56:38 +0300

    Order Invoice

--- app/Http/Controllers/User/AllUserController.php
+++ app/Http/Controllers/User/AllUserController.php
@@ -8,6 +8,7 @@ use App\Models\OrderItem;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Barryvdh\DomPDF\Facade\Pdf;
 
 class AllUserController extends Controller
 {
@@ -37,5 +38,17 @@ class AllUserController extends Controller
         $order = Order::with('division','district','state','user')->where('id',$order_id)->where('user_id',Auth::id())->first();
         $orderItem = OrderItem::with('product')->where('order_id',$order_id)->orderBy('id','DESC')->get();
         return view('frontend.order.order_details',compact('order','orderItem'));
+    }
+
+
+    public function UserOrderInvoice($order_id)
+    {
+        $order = Order::with('division','district','state','user')->where('id',$order_id)->where('user_id',Auth::id())->first();
+        $orderItem = OrderItem::with('product')->where('order_id',$order_id)->orderBy('id','DESC')->get();
+        $pdf = Pdf::loadView('frontend.order.order_invoice', compact('order','orderItem'))->setPaper('a4')->setOption([
+            'tempDir' => public_path(),
+            'chroot'  => public_path(),
+        ]);
+        return $pdf->download('invoice.pdf');
     }
 }
